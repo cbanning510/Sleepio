8f658b2956bfdf4ca0749aa1fe3144c111be267e
Chris <chrisbanning510@gmail.com>
2022-04-03 04:25:34 -0400

    add Done button to picker

--- components/PickerComponent.js
+++ components/PickerComponent.js
@@ -1,31 +1,46 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Button, Text, TouchableOpacity, View} from 'react-native';
 
 import {Picker} from '@react-native-picker/picker';
 
 const PickerComponent = ({callback, items, value, setValue}) => {
   return (
-    <Picker
-      style={styles.container}
-      selectedValue={value}
-      onValueChange={(itemValue, itemIndex) => {
-        setValue(itemValue);
-        callback();
-      }}>
-      {items.map((d, i) => (
-        <Picker.Item
-          color="white"
-          key={d + i}
-          label={d.toString()}
-          value={d.toString()}
-        />
-      ))}
-    </Picker>
+    <View>
+      <TouchableOpacity onPress={callback} style={styles.doneContainer}>
+        <Text style={styles.doneText}>Done</Text>
+      </TouchableOpacity>
+      <Picker
+        style={styles.container}
+        selectedValue={value}
+        onValueChange={(itemValue, itemIndex) => {
+          setValue(itemValue);
+        }}>
+        {items.map((d, i) => (
+          <Picker.Item
+            color="white"
+            key={d + i}
+            label={d.toString()}
+            value={d.toString()}
+          />
+        ))}
+      </Picker>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {width: '100%', textAlign: 'left', backgroundColor: 'dodgerblue'},
+  doneContainer: {
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+  },
+  doneText: {
+    fontSize: 18,
+    marginRight: 10,
+    position: 'relative',
+    bottom: 12,
+    color: 'dodgerblue',
+  },
 });
 
 export default PickerComponent;
